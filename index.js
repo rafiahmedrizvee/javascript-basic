876af125f44ee9165ba840a3c8e6e8b33be22477
rafiahmedrizvee <rizvirafi7@gmail.com>
2024-11-17 20:11:53 +0600

    java_basic

---
+++ index.js
@@ -0,0 +1,97 @@
+var number=45;
+var name ="Rizvee";
+var price=99;
+console.log(name,number,price);
+
+var isRaining=false;
+console.log(isRaining);
+var girlFriend="alia bhat";
+console.log(girlFriend);
+
+
+var PIE = 3.1416;
+console.log(PIE);
+
+
+var girlFriend = "maya";
+girlFriend = "sumaiya";
+console.log(girlFriend);
+
+var productPrice = 99.99;
+productPrice = 100;
+console.log(productPrice);
+
+var beefPrice = 750;
+var chickenPrice = 310;
+var totalPrice = beefPrice + chickenPrice;
+console.log(totalPrice);
+
+var beefPrice = 750;
+var chickenPrice = 310;
+var totalPrice = beefPrice - chickenPrice;
+console.log(totalPrice);
+
+var beefPrice = 750;
+var chickenPrice = 310;
+var totalPrice = beefPrice * chickenPrice;
+console.log(totalPrice);
+
+
+var beefPrice = 750;
+var chickenPrice = 310;
+var totalPrice = beefPrice / chickenPrice;
+console.log(totalPrice);
+
+//var totalPrice = beefPrice / chickenPrice;
+//console.log(totalPrice);
+
+
+var banana = 10;
+banana = banana + 2
+banana += 2
+console.log(banana);
+
+
+var age = 25;
+age += 1;
+age = age + 1;
+age++;
+console.log(age);
+
+
+var suger ='150';
+var vegatable = 200;
+var convertToNumber = parseInt(suger);
+var totalPrice = (vegatable+convertToNumber);
+console.log(totalPrice);
+
+
+
+var suger ='150.99';
+var vegatable = 200;
+var convertToNumber = parseFloat(suger);
+var totalPrice = (vegatable+convertToNumber);
+console.log(totalPrice);
+
+
+var today = new Date();
+console.log(typeof today);
+
+var today = new Date();
+console.log(today.getHours ());
+
+var today = new Date();
+console.log(typeof(girlFriend));
+
+
+
+var today = new Date();
+console.log(typeof(isRaining));
+
+var today = new Date();
+console.log(typeof(number));
+
+var today = new Date();
+console.log(typeof(price));
+
+
