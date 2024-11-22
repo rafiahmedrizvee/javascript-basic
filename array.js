3959d0057b08de45e7f77dced53583d39eeb3fba
rafiahmedrizvee <rizvirafi7@gmail.com>
2024-11-22 19:57:01 +0600

    basic-loop

---
+++ array.js
@@ -0,0 +1,166 @@
+var student = ['Rizvee', 'Rafi', 'Roxas', 'Arafat'];
+//console.log(student.length); //length
+
+var vowel = ['a', 'e', 'i', 'o', 'u']
+//console.log(vowel.length);
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+//console.log(mix.length); length number
+
+const mixArrayIndex = mix.indexOf(255)
+//console.log(mixArrayIndex);  ///index number
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+var findFiveIndex = mix[5];
+//console.log(findFiveIndex); //find index
+
+var findFiveIndex = mix[55];
+//console.log(findFiveIndex);
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+//console.log(mix);
+mix[2] = 'Looking Wow'
+mix.push('Rizvee')
+//console.log(mix); //add new item
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+
+mix.pop()
+mix.pop()
+
+//console.log(mix); //out item 
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+mix.shift();
+//console.log(mix);
+
+
+
+//console.log(10 > 5);
+//console.log(10 < 5);
+//console.log(10 == 5);
+//console.log(10 == 10);
+//console.log(10 != 5);
+//console.log(10 != 10);
+
+
+var myLove = 99;
+var herLove = 100;
+//console.log(myLove > herLove);
+//console.log(myLove == herLove);
+//console.log(myLove < herLove);
+//console.log(myLove != herLove);
+
+// goodStudy && goodPerson
+// hasMoney || handsome || programmer
+
+
+// is food ready == true // khabo
+
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+   //console.log('Ami khabar khabo');
+}
+
+// if statement
+
+var iphone = 160000;
+var myBudget = 90000;
+
+
+if (iphone < myBudget) {
+    console.log('iphone kinbo');
+}
+
+else {
+   // console.log('luck kharaf');
+
+}  ///if /else statement
+
+
+var jobHolder = false;
+var saveMoney = 150000;
+var hasFlat = true;
+
+if (jobHolder == true && saveMoney < 200000){
+   // console.log('biye kora hobe');
+    
+}
+else {
+//    console.log('biye hobe na');
+    
+}
+
+if (jobHolder == true || saveMoney < 200000){
+   // console.log('biye kora hobe');
+     
+ }
+ else {
+  //  console.log('biye hobe na');
+     
+ }
+ 
+
+ if ((jobHolder == true && saveMoney > 200000) || hasFlat == true){
+    console.log('Jao biye kore neo');
+     
+ }
+ else {
+    console.log('biye korar dorkar nai');
+     
+ }
+
+ if ((jobHolder == true && saveMoney > 200000) || hasFlat == true){
+   console.log('Jao biye kore neo');
+    
+}
+else {
+   console.log('biye korar dorkar nai');
+    
+}
+ 
+var money =100;
+var danis =50;
+var batterBan=10;
+var toast = 8;
+var wellPacked = true;
+
+if (danis <money) {
+   console.log("danis diye khamu");
+}
+else if (batterBan < money) {
+   console.log("batterBan diye khamu");
+}
+else if (toast < money) {
+   console.log("toast diye khamu");
+}
+else {
+   console.log("batasha diye khamu");
+   
+}
+
+
+//if...else if.. statement.
+
+var tourDestination = [ 'coxs bazar','nepal','srilanka','island'];
+if(tourDestination[0] == 'nepal'){
+   console.log('Nepal giye pahar dkhbo');
+   
+}
+else if(tourDestination[1] == 'nepal') {
+   console.log('Nepal giye pahar dkhbo');
+   
+
+}
+else if (tourDestination[2] == 'lonka'){
+   console.log('srilanka jabo');
+   
+}
+else{
+   console.log('tk nai');
+   
+}
+///if ...else if statement.
+
+
---
+++ loop.js
@@ -0,0 +1,61 @@
+/*  var sweetGiven =0;
+ while ( sweetGiven < 5 ) {
+    console.log("i eat sweet");
+    sweetGiven++
+ } */
+/*  var sweetGiven =0;
+ while ( sweetGiven < 5 ) {
+    sweetGiven++
+    console.log("i eat sweet");
+    
+ } */
+/* var number = 0;
+ while (number <=10){
+    console.log(number);
+    number++
+ }
+     */
+ /*    var number = 0;
+     while (number <=10){
+        number++
+        console.log(number);
+ } */  /* while loop 1-10  */
+
+
+/*  var number = 2;
+ while (number <=100){
+ console.log(number);
+ number += 2
+}
+while loop even number */ 
+
+/* for (var number =0; number <=20; number++){
+    console.log(number);
+    
+}
+    for loop 1-20number// 
+ 
+/*  */
+/* for (var i =0; i<=10; i += 2) {
+    console.log(i);
+    
+}
+     
+even number for loop*/
+/* 
+var number =[20,30,40,50,60,70,80];
+var index =number.indexOf(50)
+console.log(index);
+ */   //indexof 
+
+
+var numbers = [10,20,30,40,50,60,70];
+for (var i =0; i < numbers.length; i++) {
+    var element = numbers[i]
+    console.log(element);
+    
+}
+
+
+
+
