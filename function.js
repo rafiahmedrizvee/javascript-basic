423821e741fbe34a5f511b01206d6ee7bc963000
rafiahmedrizvee <rizvirafi7@gmail.com>
2024-11-24 21:04:34 +0600

    basic of javascript

---
+++ function.js
@@ -0,0 +1,82 @@
+/* 
+
+function startFan(){
+    console.log('fan colbe');
+    
+}
+startFan()
+
+ */
+/* 
+function brigeApple(taka){
+    console.log(taka,'line 8');
+    
+    console.log('apple kinbo');
+    
+}
+brigeApple(100) */
+
+/* 
+
+function bringeApple(taka) {
+var applePrice = 20;
+var totalAppleQuantity = taka/ applePrice;
+console.log(totalAppleQuantity);
+
+
+}
+bringeApple(100) */
+
+/* function bringeApple(taka) {
+    var applePrice = 20;
+    var totalAppleQuantity = taka/ applePrice;
+    return totalAppleQuantity
+    
+    }
+
+    var rafi =bringeApple(100)
+    console.log(rafi);
+
+    var ali = bringeApple(520)
+    console.log(ali);
+     */
+    
+/*     
+ function dollarToTk(dollar) {
+    var taka = dollar * 119.44;
+    return taka
+ }
+
+ var convertedTaka = dollarToTk(50)
+ console.log(convertedTaka);
+  */
+
+/* 
+ function addTwoNumber(num1,num2) {
+    console.log(num1);
+    console.log(num2);
+    
+    
+ }
+ addTwoNumber(20,30) */
+
+/* 
+ function addTwoNumber(num1,num2) {
+    var total = num1 + num2;
+    return total
+ }
+ var result = addTwoNumber(20977999, 63536663);
+ console.log(result);
+  */
+
+/* 
+ function addTwoNumber(num1,num2) {
+    var total = num1 * num2;
+    return total
+ }
+ var result = addTwoNumber(209, 635);
+ console.log(result);
+ */
+
+
+ 
---
+++ object.js
@@ -0,0 +1,54 @@
+/* var specialOne = {
+    name:'rizvee',
+    age:27,
+    handsome:true,
+    tall:'yes lomba'
+}
+
+console.log(specialOne); */
+
+/* 
+var specialOne = {
+    name:'rizvee',
+    age:27,
+    handsome:true,
+    tall:'yes lomba'
+}
+
+console.log(specialOne.name); */
+
+/* 
+var specialOne = {
+    name:'rizvee',
+    age:27,
+    handsome:true,
+    tall:'yes lomba'
+}
+
+console.log(specialOne.age);
+ */
+
+/* 
+var laptop = {
+    name : 'Hp Elitebook 840 g6',
+    price: 500,
+    color:'Grey'
+
+}
+laptop.name = 'Elitebook'
+
+console.log(laptop);
+ */
+
+/* 
+var laptop = {
+    name : 'Hp Elitebook 840 g6',
+    price: 500,
+    color:'Grey'
+
+}
+laptop.name = 'Elitebook'
+laptop["color"]= "white"
+
+console.log(laptop);
+ */
